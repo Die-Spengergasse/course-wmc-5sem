7587814c643073fe9b53afcd3d8e62b8f5afb791
Michael Schletz <schletz@spengergasse.at>
2025-03-16 13:55:15 +0100

    Update reducer

--- 30_TodoApp/Delete/src/app/categories/CategoryList.tsx
+++ 30_TodoApp/Delete/src/app/categories/CategoryList.tsx
@@ -7,27 +7,32 @@ import styles from "./CategoryList.module.css";
 import CategoryDelete from "./CategoryDelete";
 
 // Discriminated unions in typescript
-type EditDeleteReducerAction =
-  | { type: null; category: null }                   // Wenn actionType null ist, ist category null
-  | { type: "edit" | "delete"; category: Category }; // Wenn actionType "edit" oder "delete" ist, ist category vom Typ Category
-type EditDeleteState =
-  | { actionType: null; category: null }
-  | { actionType: "edit" | "delete"; category: Category };
+type ReducerAction =
+  | { resetState: true }
+  | { resetState?: false; intent: "edit" | "delete"; category: Category };
+type CategoryListState =
+  | { dialogType: "" }
+  | { dialogType: "error"; error: string }
+  | { dialogType: "edit" | "delete"; category: Category };
 
-function editDeleteReducer(
-  state: EditDeleteState,
-  action: EditDeleteReducerAction): EditDeleteState {
-  switch (action.type) {
+function reducer(
+  state: CategoryListState,
+  action: ReducerAction): CategoryListState {
+  if (action.resetState) return { dialogType: "" }
+  switch (action.intent ) {
     case "edit":
-      return { category: action.category, actionType: "edit" };
+      if (action.category.isVisible)
+        return { category: action.category, dialogType: "edit" };
+      else
+        return { dialogType: "error", error: "You cannot edit an invisible category." }
     case "delete":
-      return { category: action.category, actionType: "delete" };
+      return { category: action.category, dialogType: "delete" };
     default:
-      return { category: null, actionType: null };
+      return { dialogType: "" };
   }
 }
 export default function CategoryList({ categories }: { categories: Category[] }) {
-  const [selectedCategory, selectedCategoryDispatch] = useReducer(editDeleteReducer, { category: null, actionType: null });
+  const [state, dispatcher] = useReducer(reducer, { dialogType: "" });
   const categoryEditRef = useRef<CategoryEditRef>(null);
   return (
     <div className={styles.categories}>
@@ -38,37 +43,46 @@ export default function CategoryList({ categories }: { categories: Category[] })
               <h2>{category.name}</h2>
               <span
                 className={styles.editIcon}
-                onClick={() => selectedCategoryDispatch({ type: "edit", category: category })}
+                onClick={() => dispatcher({ intent: "edit", category: category })}
                 title="Edit"
               >
                 ✏️
               </span>
               <span
                 className={styles.editIcon}
-                onClick={() => selectedCategoryDispatch({ type: "delete", category: category })}
+                onClick={() => dispatcher({ intent: "delete", category: category })}
                 title="Delete"
               >
                 🗑️
               </span>
             </div>
             <p>{category.description}</p>
+            <p>Visible: {category.isVisible ? "yes" : "no"}</p>
           </li>
         ))}
       </ul>
 
-      {selectedCategory.actionType == "edit" && (
-        <ModalDialog title={`Edit ${selectedCategory.category.name}`}
+      {state.dialogType == "error" && (
+        <ModalDialog title="Error"
+          onOk={() => dispatcher({ resetState: true })}
+          onCancel={() => dispatcher({ resetState: true })}>
+          {state.error}
+        </ModalDialog>
+
+      )}
+      {state.dialogType == "edit" && (
+        <ModalDialog title={`Edit ${state.category.name}`}
           onOk={() => categoryEditRef.current?.startSubmit()}
-          onCancel={() => selectedCategoryDispatch({ type: null, category: null })}>
-          <CategoryEdit category={selectedCategory.category}
+          onCancel={() => dispatcher({ resetState: true })}>
+          <CategoryEdit category={state.category}
             ref={categoryEditRef}
-            onSubmitted={() => selectedCategoryDispatch({ type: null, category: null })} />
+            onSubmitted={() => dispatcher({ resetState: true })} />
         </ModalDialog>
       )}
-      {selectedCategory.actionType == "delete" && (
-        <CategoryDelete category={selectedCategory.category}
-          onCancel={() => selectedCategoryDispatch({ type: null, category: null })}
-          onDeleted={() => selectedCategoryDispatch({ type: null, category: null })} />
+      {state.dialogType == "delete" && (
+        <CategoryDelete category={state.category}
+          onCancel={() => dispatcher({ resetState: true })}
+          onDeleted={() => dispatcher({ resetState: true })} />
       )}
     </div>
   );
